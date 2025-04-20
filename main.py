6aeaa0245ce34bc9df33bb52dd4433c31e613c19
nitinyadav798906 <nitinyadav798906@gmail.com>
2025-04-21 01:21:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,12 +44,12 @@ cookies_file_path = os.getenv("COOKIES_FILE_PATH", "youtube_cookies.txt")
 
     
 # Define the owner's user ID
-OWNER_ID = 1577263404,1001936426958 # Replace with the actual owner's user ID
+OWNER_ID = 1577263404 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
-SUDO_USERS = [1577263404,1001936426958]
+SUDO_USERS = [1577263404]
 
-AUTH_CHANNEL = 1001936426958
+AUTH_CHANNEL = -1001936426958
 
 # Function to check if a user is authorized
 def is_authorized(user_id: int) -> bool:
